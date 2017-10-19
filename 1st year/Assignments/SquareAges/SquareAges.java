b9f5b2ab5ef48cb9bf63d9a89ef79ea6f0e4e734
Luke Hackett <lhackett@tcd.ie>
2017-10-19 18:43:46 +0100

    Changed constants
    Altered constants to easier understand the logic

--- 1st year/Assignments/SquareAges/SquareAges.java
+++ 1st year/Assignments/SquareAges/SquareAges.java
@@ -3,17 +3,21 @@ import javax.swing.JOptionPane;
 
 public class SquareAges
 {
-    public static final int MIN_YEAR = 1894;
-    public static final int MAX_YEAR = 2140;
+	public static final int CURRENT_YEAR = 2017;
+    public static final int MAX_AGE = 123;
 
     public static void main(String[] args)
     {
-        for(int age = 1; age <= 123; age++)
+    	int maxYear = CURRENT_YEAR + MAX_AGE;
+    	int minYear = CURRENT_YEAR - MAX_AGE;
+    	
+        for(int age = 1; age <= MAX_AGE; age++)
         {
             int squaredAge = age * age;
-            if(squaredAge <= (MAX_YEAR - age) && squaredAge >= (MIN_YEAR + age))
+            if(squaredAge <= (maxYear - age) && squaredAge >= (minYear + age))
             {
-              JOptionPane.showMessageDialog(null, "If you are age " + age + ", it is possible you will be alive in the year that is your age squared. This year is " + squaredAge);
+              JOptionPane.showMessageDialog(null, "If you are age " + age + ", it is possible you will be alive in the year "
+              														+ "that is your age squared. This year is " + squaredAge);
             }
         }
     }
