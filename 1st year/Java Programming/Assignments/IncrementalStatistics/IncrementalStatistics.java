5f9ae2cc389d055d51cc1156dd819bbdb361628e
LukeHackett12 <luke.hackett12@gmail.com>
2017-10-31 18:06:06 +0000

    ll

--- 1st year/Java Programming/Assignments/IncrementalStatistics/IncrementalStatistics.java
+++ 1st year/Java Programming/Assignments/IncrementalStatistics/IncrementalStatistics.java
@@ -1,3 +1,23 @@
+/* SELF ASSESSMENT
+   1. Did I use easy-to-understand meaningful variable names?
+       Mark out of 10: 10
+   2. Did I format the variable names properly (in lowerCamelCase)?
+       Mark out of 5: 5
+   3. Did I indent the code appropriately?
+       Mark out of 10: 10
+   4. Did I input the numbers one at a time from the command line?
+       Mark out of 10: 10
+   5. Did I check the input to ensure that invalid input was handled appropriately?
+       Mark out of 10: 10
+   6. Did I use an appropriate while or do-while loop to allow the user to enter numbers until they entered exit/quit?
+       Mark out of 20: 20
+   7. Did I implement the loop body correctly so that the average and variance were updated and output appropriately?
+       Mark out of 30: 30
+   8. How well did I complete this self-assessment?
+       Mark out of 5: 5
+   Total Mark out of 100 (Add all the previous marks):
+*/
+
 import java.util.Scanner;
 
 public class IncrementalStatistics {
