1f18458a2f7edd48549d5da89f3bfb2e5a4436aa
Luke Hackett <lhackett@tcd.ie>
2017-11-08 00:09:15 +0000

    Remade Lottery wayyy better

--- 1st year/Java Programming/Random/LukesLottery.java
+++ 1st year/Java Programming/Random/LukesLottery.java
@@ -1,77 +1,107 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public class Lottery {
-	public static void main(String[] args) {
-		
-		System.out.print("Please pick three numbers that you would like to enter the loterry with: ");
-		
-		Scanner inputScanner = new Scanner(System.in);
-		int guess1 = inputScanner.nextInt();
-		while(guess1 > 45 || guess1 < 1) {
-			System.out.print("You already have that number or it is not within our range, pick another: ");
-			//inputScanner = new Scanner(System.in);
-			guess1 = inputScanner.nextInt();
-		}
-		int guess2 = inputScanner.nextInt();
-		while (guess1 == guess2 || guess2 > 45 || guess2 < 1) {			
-			System.out.print("You already have that number or it is not within our range, pick another: ");
-			//inputScanner = new Scanner(System.in);
-			guess2 = inputScanner.nextInt();
-		}
-		int guess3 = inputScanner.nextInt();	
-		while (guess1 == guess3 || guess2 == guess3 || guess3 > 45 || guess2 < 1) {
-			System.out.print("You already have that number or it is not within our range, pick another: ");
-			//inputScanner = new Scanner(System.in);
-			guess2 = inputScanner.nextInt();
-		}
-		inputScanner.close();
-		
-		int guessNums[] = {guess1, guess2, guess3};
-		//Calculate random numbers
-		int winNums[] = {0, 0, 0};
-		
-		for(int i = 0; i < 3; i++) {
-			
-			boolean _isValid = true;
-			int tempRandom = (int)(Math.random() * 45 + 1);
-			
-			for(int j = 0; j < 3; j++) {
-				if(tempRandom != winNums[j])
-					_isValid = true;
-				else {
-					_isValid = false;
-					j = 3;
+public class LukesLottery {
+	public static void main(String[] args){
+		//Start game
+		boolean quit = false;
+		Scanner input = new Scanner(System.in);
+
+		while(!quit){
+			//Start Input
+			boolean valid = false;
+			int[] guess = new int[3];
+			while(!valid){
+				boolean withinBounds = true;
+				boolean differentNums = true;
+				System.out.print("Please select three numbers between 1 and 45 for your ticket: ");
+				for(int i = 0; i < 3; i++){
+					if(input.hasNextInt()){
+						guess[i] = input.nextInt();
+						if(guess[i] >= 1 && guess[i] <= 45){
+							valid = true;
+							for(int j = 0; j < i; j++){
+								if(guess[i] == guess[j]){
+									differentNums = false;
+								}
+							}
+						}
+						else if(guess[i] < 1 || guess[i] > 45){
+							withinBounds = false;
+						}
+						else {
+							valid = false;
+						}
+					}
+					else if(input.hasNext("quit")){
+						System.out.print("Thank you for playing");
+						System.exit(0);
+					}
+
+					if(!withinBounds){
+						System.out.println("Sorry one of your numbers was not within 1 and 45, please try again");
+						valid = false;
+						i = 3;
+					}
+					else if(!valid){
+						System.out.println("Sorry one of your numbers was wrong, please try again");
+						valid = false;
+						i = 3;
+					}
+					else if(!differentNums){
+						System.out.println("Sorry some of your numbers appeared to be the same, please try again");
+						valid = false;
+						i = 3;
+					}
 				}
 			}
-			
-			if(_isValid)
-				winNums[i] = tempRandom;
-			else
-				i--;
-		}
-		
-		int winMatch[] = {0, 0, 0};
-		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(guessNums[i] == winNums[j])
-					winMatch[i] = guessNums[i];
+			//End input
+
+			//Start ticket generation
+			Random generator = new Random();
+			int[] ticket = new int[3];
+			for(int i = 0; i < 3; i++){
+				ticket[i] = generator.nextInt(45) + 1;
+				for(int j = 0; j < i; j++){
+					if(ticket[j] == ticket [i]){
+						ticket[i] = generator.nextInt(45) + 1;
+					}
+				}
+			}
+			//End ticket generation
+
+			//Start comparison
+			int correctGuesses = 0;
+			for(int i = 0; i < 3; i++){
+				for(int j = 0; j < 3; j++){
+					if(guess[i] == ticket[j]){
+						correctGuesses++;
+					}
+				}
+			}
+			//End comparison
+
+			//Print outcome
+			if(correctGuesses == 3){
+				System.out.print("You have won!! Do you want to play again(yes/no): ");
+
+				String again= input.next();
+				if(!again.equals("yes")){
+					System.out.println("Ok, thanks for playing");
+					System.exit(0);
+				}
+
+			}
+			else{
+				System.out.print("You had " + correctGuesses + " correct guesses. Do you want to play again(yes/no): ");
+
+				String again= input.next();
+				if(!again.equals("yes")){
+					System.out.println("Ok, thanks for playing");
+					System.exit(0);
+				}	
 			}
 		}
-		
-		if(winMatch[0] != 0 && winMatch[1] != 0 && winMatch[2] != 0)
-			System.out.println("You have won! The numbers that matched were: " + winMatch[0] + "," + winMatch[1] + "," + winMatch[2]);
-		else {
-			System.out.print("You have lost, the numbers that matched were ");
-			if(winMatch[0] != 0) {
-				System.out.print(winMatch[0] + ",");
-			}
-			if(winMatch[1] != 0) {
-				System.out.print(winMatch[1]  + ",");
-			}
-			if(winMatch[2] != 0) {
-				System.out.print(winMatch[2]);
-			}
-		}
+		input.close();
 	}
 }
