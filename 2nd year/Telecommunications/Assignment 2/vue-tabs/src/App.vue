d80e9e3957c55366736b254cfc56c4ec17c6d792
Luke Hackett <lhackett@tcd.ie>
2018-12-01 20:19:43 +0000

    test

--- 2nd year/Telecommunications/Assignment 2/vue-tabs/src/App.vue
+++ 2nd year/Telecommunications/Assignment 2/vue-tabs/src/App.vue
@@ -42,7 +42,25 @@ export default {
   },
   components: {
     'documents': documents
-  }
+  },
+   rules: [
+      // ... other rules omitted
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // enable CSS Modules
+              modules: true,
+              // customize generated class names
+              localIdentName: '[local]_[hash:base64:8]'
+            }
+          }
+        ]
+      }
+    ]
 }
 </script>
 
