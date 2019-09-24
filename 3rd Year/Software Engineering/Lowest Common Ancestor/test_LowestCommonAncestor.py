02b2bb160a56e47c446c13dfc842eb6debcd7bb5
Luke Hackett <lhackett@tcd.ie>
2019-09-24 15:20:09 +0100

    All tests passing and 100 percent coverage in pytest cov report

--- 3rd Year/Software Engineering/Lowest Common Ancestor/test_LowestCommonAncestor.py
+++ 3rd Year/Software Engineering/Lowest Common Ancestor/test_LowestCommonAncestor.py
@@ -23,3 +23,18 @@ class TestLowestCommonAncestor:
         lca = LowestCommonAncestor.findLCA(root, 4, 5)
 
         assert lca is None
+
+    def test_p_is_lowest(self):
+        root = Node(3)
+        root.left = Node(5)
+        root.right = Node(1)
+        root.left.left = Node(6)
+        root.left.right = Node(2)
+        root.right.left = Node(0)
+        root.right.right = Node(8)
+        root.left.right.left = Node(7)
+        root.left.right.right = Node(4)
+
+        lca = LowestCommonAncestor.findLCA(root, 5, 4)
+
+        assert lca.value is 5
