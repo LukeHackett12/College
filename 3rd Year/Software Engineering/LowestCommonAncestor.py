e9381730dfc743ffe4a067bfc2a386f2839fff77
Luke Hackett <lhackett@tcd.ie>
2019-09-24 14:59:54 +0100

    Initial implimentation of LowestCommonAncestor problem

---
+++ 3rd Year/Software Engineering/LowestCommonAncestor.py
@@ -0,0 +1,17 @@
+from binarytree import Node
+
+
+def findLCA(root, n1, n2):
+    if root is None:
+        return None;
+
+    if root.value == n1 or root.value == n2:
+        return root
+
+    left_lca = findLCA(root.left, n1, n2)
+    right_lca = findLCA(root.right, n1, n2)
+
+    if left_lca and right_lca:
+        return root
+
+    return left_lca if left_lca is not None else right_lca
