f3f20f2e1aff28f852c9d12a2f97fae6a4985d88
Luke Hackett <lhackett@tcd.ie>
2019-09-29 16:59:29 +0100

    Exercise 1 finished I hope

--- 3rd Year/Symbolic Programming/Assignment 1/Exercise1.pl
+++ 3rd Year/Symbolic Programming/Assignment 1/Exercise1.pl
@@ -1,19 +1,32 @@
 numeral(0).
 numeral(s(X)) :- numeral(X).
+numeral(p(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
+add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
-add2(X+Z,Y,R) :- add((X),(Z),A),
-			add(A,Y,R).
+/* Start add2 */
+/* add2 on successor */
 
-add2(X,Y+Z,R) :- add((Y),(Z),A),
-			add(X,A,R).
-			
-add2(0,X+Y,R) :- add((X),(Y),R).
-			
-add2(X+Y,P+Q,R) :- add((X),(Y),A),
-				add((P),(Q),B),
-				add(A,B,R).
+add2(s(X+Y),Z,s(R)) :- add2(X+Y,Z,R).
+add2(X,s(Y+Z),s(R)) :- add2(X,Y+Z,R).
+add2(s(X+Y),s(P+Q),s(s(R))) :- add2(X+Y,P+Q,R).
 
+/* end add2 successor */
+
+/* add2 general predicates */
+
+add2(X+Z,Y,R) :- add(X,Z,A),
+			add2(A,Y,R).
+add2(X,Y+Z,R) :- add(Y,Z,A),
+			add2(X,A,R).
+add2(X+Y,P+Q,R) :- add(X,Y,A),
+				add(P,Q,B),
+				add2(A,B,R).
+
+add2(X, Y, Z) :- add(X, Y, Z).
+
+/* end add2 general predicates */
+/* End add2 */
