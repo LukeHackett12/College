945a6357f5a195d6e5d109bba9502495c2f7684f
Luke Hackett <lhackett@tcd.ie>
2019-10-02 12:53:26 +0100

    Assignment for prolog updated

--- 3rd Year/Symbolic Programming/Assignment1.pl
+++ 3rd Year/Symbolic Programming/Assignment1.pl
@@ -2,12 +2,24 @@ numeral(0).
 numeral(s(X)) :- numeral(X).
 numeral(p(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
+numeral(-X) :- numeral(X).
 
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
 /* Start add2 */
+
+/* add2 on - */
+
+add2(-X,Y,Z) :- minus(X,A),
+                add2(A,Y,Z).
+
+add2(X,-Y,Z) :- minus(Y,A),
+                add2(X,A,Z).
+
+/* end add2 - */
+
 /* add2 on successor */
 
 add2(s(X+Y),Z,s(R)) :- add2(X+Y,Z,R).
@@ -15,6 +27,16 @@ add2(X,s(Y+Z),s(R)) :- add2(X,Y+Z,R).
 add2(s(X+Y),s(P+Q),s(s(R))) :- add2(X+Y,P+Q,R).
 
 /* end add2 successor */
+
+/* add2 on predecessor */
+
+add2(p(s(X)),Y,Z) :- add2(X,Y,Z).
+add2(X, p(s(Y)),Z) :- add2(X,Y,Z).
+
+add2(s(p(X)),Y,Z) :- add2(X,Y,Z).
+add2(X,s(p(Y)),Z) :- add2(X,Y,Z).
+
+/* end add2 predecessor */
 
 /* add2 general predicates */
 
@@ -30,3 +52,11 @@ add2(X, Y, Z) :- add(X, Y, Z).
 
 /* end add2 general predicates */
 /* End add2 */
+
+/* Start Minus */
+minus(0,0).
+minus(s(p(X)),Y) :- minus(X,Y).
+minus(p(s(X)),Y) :- minus(X,Y).
+minus(s(X),p(Y)) :- minus(X,Y).
+minus(p(X),s(Y)) :- minus(X,Y).
+/* End Minus */
